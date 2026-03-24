9b45821a9315c04b7181dcbf87d76b128fdf2c21
Victor Iatco <victor.iatco@appsfactory.de>
2026-03-24 12:29:02 +0200

    Fix alembic version_num column width to VARCHAR(64)

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config, pool, String
 
 from alembic import context
 
@@ -66,7 +66,8 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata,
+            version_table_pk_type=String(64),
         )
 
         with context.begin_transaction():
